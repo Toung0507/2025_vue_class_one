d4601227ef79a2e086173b6ac9e9652d681108bc
Toung <spexial110@gmail.com>
2025-08-27 00:33:11 +0800

    update code

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,13 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup>// 且因為 vite 的原因，所以不需要特別寫出 return
+import { ref } from 'vue'; // 需要透過引用的方式，取出方法 from 'vue' 的字體大小寫要跟 main.js 的相同
+const num = ref(2);
+function clickMe() {
+  num.value++;
+};
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  {{ num }}
+  <button type="button" v-on:click="clickMe">點我</button>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
