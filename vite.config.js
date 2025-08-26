7c28d2980aa8abeaceb0b491a8dace8c0a702aa9
Toung <spexial110@gmail.com>
2025-08-27 01:16:50 +0800

    setting pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/2025_vue_class_one/', 
   plugins: [
     vue(),
     vueDevTools(),
